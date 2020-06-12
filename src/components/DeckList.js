0c4c117218aba3a1cd64827bd0375de39a0782e8
tdyleuth <tony.dyleuth@gmail.com>
2020-06-11 20:43:25 -0700

    Deckbuilder Day 2

---
+++ src/components/DeckList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DeckList = (props) => {
+    return (    
+    <div id="deck">
+        <h3>Your deck so far:</h3>
+    </div>
+    
+    );
+}
+
+export default DeckList;
