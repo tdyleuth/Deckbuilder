85017fa274af656e5d00e3780677de734d2efcb1
tdyleuth <tony.dyleuth@gmail.com>
2020-06-11 20:46:15 -0700

    Day 2 Complete

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+  fetchCards,
+} from './api'
+
+import axios from 'axios';
+
+import {
+  DeckList,
+  SearchBar,
+  SearchResults
+} from './components';
+
+const App = () => {
+
+
+  const [results, setResults] = useState([]);
+
+    return (
+      <div id="app">
+        <SearchBar setResults={ setResults } />
+        <SearchResults results={ results } />
+        <DeckList />
+      </div>
+  );
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
+
